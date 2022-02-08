94e280cc58e4f5846c1a92293cceec8578845ae6
kj-duggu <harshvardhan152k@gmail.com>
2022-02-08 11:02:29 +0530

    Third commit

---
+++ basicprog/src/basicprog/Hellok.java
@@ -0,0 +1,11 @@
+package basicprog;
+
+public class Hellok {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello kalpesh");
+
+	}
+
+}
---
+++ basicprog/src/basicprog/P9MultiT.java
@@ -0,0 +1,14 @@
+package basicprog;
+
+public class P9MultiT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 5;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+	}
+
+}
